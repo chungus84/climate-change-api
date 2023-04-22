44803828e169059066dcad5536f9c1b0bd2adace
Kenichi Beveridge <kenbev84@gmail.com>
2023-04-22 09:37:11 +0100

    Set both home and /news path and created and array of newspapers to loop over

--- index.js
+++ index.js
@@ -9,5 +9,63 @@ const cheerio = require('cheerio');
 // calling express saved as const app
 const app = express();
 
+// array of newspapers to scrape
+
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+    base: ''
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.com/environment/climate-crisis',
+    base: ''
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change',
+    base: 'https://www.telegraph.co.uk'
+  },
+  {
+    name: 'nytimes',
+    address: 'https://www.nytimes.com/international/section/climate',
+    base: 'https://www.nytimes.com'
+  },
+
+]
+
+// array to save hrefs of articles
+const articles = [];
+
+newspapers.forEach(newspaper => {
+  axios.get(newspaper.address)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text().replace(/\s+/g, ' ');
+      const url = $(this).attr('href');
+
+      articles.push({
+        title,
+        url: newspaper.base + url,
+        source: newspaper.name
+      })
+    });
+  });
+});
+
+// set routing for home path
+app.get('/', (req, res) => {
+  res.json("Welcome to my Climate Change News API");
+});
+
+// set /news route to guardian website to scrape climate change news using axios and cheerio
+app.get('/news', (req, res) => {
+  res.json(articles);
+});
+
 // express (app) listen for PORT 8000 with a backend callback
 app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`));
